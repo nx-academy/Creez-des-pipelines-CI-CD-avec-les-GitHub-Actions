ae50b9c55421ca9bd42060f548fce9d63580db1e
Thomas Dimnet <thomas.dimnet@gmail.com>
2023-07-28 15:18:25 +0200

    (Core) Setting up project (#1)
    * Cleaning up project
    
    * Setting up vite and react
    
    * Creating ReactApp
    
    * Adding test files
    
    * Updating .gitignore
    
    * Setting up testing lib

--- pages/about.tsx
+++
@@ -1,9 +0,0 @@
-export default function Page() {
-  const name = 'thomas'
-
-  return (
-    <main>
-      <h1>About!</h1>
-    </main>
-  )
-}
---
+++ src/App.test.jsx
@@ -0,0 +1,7 @@
+import {describe, it, expect} from "vitest";
+
+describe('App Test Suite', () => {
+  it('should be true', () => {
+    expect(true).toBe(true)
+  })
+})

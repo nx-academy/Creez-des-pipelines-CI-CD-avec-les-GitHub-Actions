868bc7903dc8d75f19d55dd0cc60f23ed41c5ef9
Thomas Dimnet <thomas.dimnet@gmail.com>
2023-07-28 15:15:31 +0200

    Setting up testing lib

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/',
   plugins: [react()],
-  root: './src'
+  root: './src',
+  test: {
+    environment: 'happy-dom'
+  }
 })
 

a888ec0c4b82c82b504cb1b58505c7bde3a220dc
Thomas Dimnet <thomas.dimnet@gmail.com>
2023-09-26 10:33:53 +0200

    Removing error

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,4 @@
 function Page() {
-
-  const firstName = "thomas"
-
   return (
     <main className="flex items-center justify-center min-h-screen w-screen">
       <h1 data-testid="title">Hello, World!</h1>

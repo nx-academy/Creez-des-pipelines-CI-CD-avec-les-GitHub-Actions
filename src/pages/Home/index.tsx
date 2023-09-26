18d7c9426ded07399fffd5fc7e8cb15cb19ac424
Thomas Dimnet <thomas.dimnet@gmail.com>
2023-09-26 11:15:23 +0200

    Adding second failed step

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,6 @@
 function Page() {
+  const firstName = "Thomas"
+
   return (
     <main className="flex items-center justify-center min-h-screen w-screen">
       <h1 data-testid="title">Hello, World!</h1>

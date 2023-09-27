133a68a4f81049c901161a3867aab673e9e45f9e
Thomas Dimnet <thomas.dimnet@gmail.com>
2023-09-27 11:49:20 +0200

    Trying to deploy before screencast

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/',
+  base: 'https://nx-academy.github.io/Creez-des-pipelines-CI-CD-avec-les-GitHub-Actions/',
   plugins: [react()],
   root: './src',
   test: {
